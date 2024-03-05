857d752247d5aa8c38ec89fc3493b7ff6174f9fb
김정호 <jeongho@charlie.kim>
2024-03-05 16:56:01 +0900

    Version Up 2.0.0c

--- kocomRS485/run.sh
+++ kocomRS485/run.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
 SHARE_DIR=/share/kocom
+VERSION=2.0.0c
 
-if [ ! -f $SHARE_DIR/rs485.2.0.0b.py ]; then
-	mkdir $SHARE_DIR
-	mv /rs485.2.0.0b.py $SHARE_DIR
-fi
+mkdir -p $SHARE_DIR
+mv /rs485-${VERSION}.py $SHARE_DIR
 /makeconf.sh
 
 echo "[Info] Run Wallpad Controller"
 cd $SHARE_DIR
-python3 $SHARE_DIR/rs485.2.0.0b.py
+python3 $SHARE_DIR/rs485-${VERSION}.py
 
 # for dev
 while true; do echo "still live"; sleep 100; done
